0cf29c807aeda355138e8c0bab323c91cf2b441f
Talha Ghauri <talhaghauridev@gmail.com>
2025-11-03 22:05:02 +0500

    add error.middleware.ts

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { env } from "../env";
+import ApiError from "../utils/ApiError";
+
+const errorMiddleware = (err: Error | ApiError, req: Request, res: Response, next: NextFunction) => {
+   console.error("[Error Middleware]", {
+      message: err.message,
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+      path: req.path,
+      method: req.method
+   });
+
+   if (err instanceof ApiError) {
+      return res.status(err.statusCode).json({
+         success: false,
+         message: err.message,
+         ...(env.NODE_ENV === "development" && { stack: err.stack })
+      });
+   }
+
+   const message = err.message || "Internal Server Error";
+   const apiError = new ApiError(500, message);
+   return res.status(apiError.statusCode).json({
+      success: false,
+      message: apiError.message,
+      ...(env.NODE_ENV === "development" && { stack: err.stack })
+   });
+};
+
+export default errorMiddleware;
